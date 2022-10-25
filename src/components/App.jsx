d7d665f00c8753c51d7626b563512bb2a7f3fc64
Ihor <ihorfront88@gmail.com>
2022-10-25 20:59:41 +0300

    add task-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
@@ -17,6 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
