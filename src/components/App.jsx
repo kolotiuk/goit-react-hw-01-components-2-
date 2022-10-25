8bcc5d1f5332496b1970711d73ef7f77c82bdef6
Ihor <ihorfront88@gmail.com>
2022-10-25 19:56:56 +0300

    add task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import Profile from './Profile/Profile';
+import Profile from './Profile';
+import Statistics from './Statistics';
 import user from 'data/user.json';
+import data from 'data/data.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
